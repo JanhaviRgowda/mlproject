7dc5988cfb36a7b5f644cfc741f561119909bc2b
JanhaviRgowda <janhavir515@gmail.com>
2025-06-14 07:59:00 +0530

    Data ingestion and Data Transformation and creating a pkl file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -27,14 +29,18 @@ class DataIngestion:
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            logging.info('Train and test data is saved')
             logging.info('Ingestion of the data is completed')
-            return (
-                self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
-                #self.ingestion_config.raw_data_path
+            
+            return (self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+                
             )
         except Exception as e:
             raise CustomException(e, sys)
 if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    obj= DataIngestion()
+    logging.info("Starting data ingestion")
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
